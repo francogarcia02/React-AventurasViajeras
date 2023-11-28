d8db6004bb348d754291955f1456882b76cac415
francogarcia02 <francoalbertogarcia2017@gmail.com>
2023-11-28 14:15:50 -0300

    Componente de alojamientos y detalles esteticos

--- src/components/Footer/Bricks/Redes/Redes.jsx
+++ src/components/Footer/Bricks/Redes/Redes.jsx
@@ -6,9 +6,9 @@ const Redes = () =>{
         <div className="Redes">
             <h2 className="Redes__title">Redes</h2>
             <div className="Redes__subcont">
-                <Link to="https://www.instagram.com/"><img className="Redes__img" src="../../../assets/img/Footer/instagram.png"/></Link>
-                <Link to="https://www.facebook.com/"><img className="Redes__img" src="../../../assets/img/Footer/facebook.png"/></Link>
-                <Link to="https://twitter.com/home"><img className="Redes__img" src="../../../assets/img/Footer/twitter.png"/></Link>
+                <Link to="https://www.instagram.com/"><img className="Redes__img" src="../../../assets/img/Footer/instagram.png" alt="instagram"/></Link>
+                <Link to="https://www.facebook.com/"><img className="Redes__img" src="../../../assets/img/Footer/facebook.png" alt="facebook"/></Link>
+                <Link to="https://twitter.com/home"><img className="Redes__img" src="../../../assets/img/Footer/twitter.png" alt="twitter"/></Link>
             </div>
         </div>
     )
---
+++ src/components/Services/Alojamientos/Item/Item.jsx
@@ -0,0 +1,20 @@
+import './Item.css';
+
+const Item = ({alojamiento}) =>{
+    const {img, nombre, descripcion, tipo} = alojamiento
+
+    console.log(nombre)
+
+    return(
+        <div className="Aloj-Item">
+            <div className="Item__img__conteiner">
+                <img className="Item__img" src={img} alt={nombre}/>
+            </div>
+            <h2 className="Item__title">{nombre}</h2>
+            <h4 className="Item__desc">{descripcion}</h4>
+            <h3 className="Item__type">{tipo}</h3>
+        </div>
+    )
+}
+
+export default Item
